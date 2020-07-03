ece15c1ea7ae1bcd857d3a718b8349038329d1a9
stasi815 <anastasia.gallardo@students.makeschool.com>
2020-07-03 13:28:48 -0700

    [update] changed settings staticfiles_dirs for django

--- decentralRize/settings.py
+++ decentralRize/settings.py
@@ -124,10 +124,13 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+# Change to this setting for local deploy
+# STATIC_ROOT = '/static/'
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIRS= [
-    os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR, 'staticfiles')
 ]
 
 # MEDIA_URL = '/static/pics/'
