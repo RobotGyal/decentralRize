eb6fee06cfd3badb62c5a365ee061da85febd05f
stasi815 <anastasia.gallardo@students.makeschool.com>
2020-07-03 16:11:14 -0700

    [update] disabled django_heroku

--- decentralRize/settings.py
+++ decentralRize/settings.py
@@ -9,7 +9,6 @@ https://docs.djangoproject.com/en/2.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
-import django_heroku
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -146,4 +145,4 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 #     import dj_database_url
 #     DATABASES = {'default': dj_database_url.config()}
 
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
