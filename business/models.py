3c0c1529f4c67dbc2141937b51180f6687e222eb
stasi815 <anastasia.gallardo@students.makeschool.com>
2020-07-14 13:19:56 -0700

    [add] genre field to business model

--- business/models.py
+++ business/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Business(models.Model):
     name = models.CharField(max_length=200)
-    genre = models(max_length=200)
+    genre = models.CharField(max_length=200)
     description = models.CharField(max_length=200)
     address = models.CharField(max_length=200)
 
