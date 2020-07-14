64e3e3835b4bd0f4e663221e52d10ec90bd8a445
Aleia <aleiaknight@gmail.com>
2020-07-13 21:03:10 -0400

    added models to page index

--- business/models.py
+++ business/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 class Business(models.Model):
     name = models.CharField(max_length=200)
+    img = models.ImageField(upload_to='pics/', blank=True)
     description = models.CharField(max_length=200)
     address = models.CharField(max_length=200)
 
