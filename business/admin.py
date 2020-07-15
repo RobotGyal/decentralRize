077c06954ae0fa097d1ec71a7220aab9a3dc322a
stasi815 <anastasia.gallardo@students.makeschool.com>
2020-07-15 00:03:21 -0700

    [add] render image in admin models

--- business/admin.py
+++ business/admin.py
@@ -3,3 +3,6 @@ from django.contrib import admin
 from .models import Business
 
 admin.site.register(Business)
+
+fields = ['image_tag']
+readonly_fields = ['image_tag']
--- business/models.py
+++ business/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.utils.html import mark_safe
+
 
 class Business(models.Model):
     name = models.CharField(max_length=200)
@@ -11,3 +13,8 @@ class Business(models.Model):
 
     def __str__(self):
         return self.name
+
+    def image_tag(self):
+            return mark_safe('<img src="/business_img/%s" width="150" height="150" />' % (self.image))
+
+    image_tag.short_description = 'Image'
