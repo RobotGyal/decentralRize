6761c9e88bd37c340d026c4e83923f565533fb7c
stasi815 <anastasia.gallardo@students.makeschool.com>
2020-07-15 01:16:33 -0700

    [done] image renders in wellness and business models in admin

--- wellness/admin.py
+++ wellness/admin.py
@@ -4,7 +4,7 @@ from .models import Wellness
 
 class WellnessAdmin(admin.ModelAdmin):
     # explicitly reference fields to be shown, note image_tag is read-only
-    fields = ( 'image_tag','name','description','image','genre', 'address',)
+    fields = ( 'image_tag','name','description','image','genre', 'location','image',)
     readonly_fields = ('image_tag',)
 
 admin.site.register(Wellness, WellnessAdmin)
