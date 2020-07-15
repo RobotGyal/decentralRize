e54bcc69f4754442cc65619f9604be3291b8f739
stasi815 <anastasia.gallardo@students.makeschool.com>
2020-07-15 00:41:55 -0700

    [update] business admin

--- business/admin.py
+++ business/admin.py
@@ -1,10 +1,9 @@
 from django.contrib import admin
-
 from .models import Business
 
+class BusinessAdmin(admin.ModelAdmin):
+    # explicitly reference fields to be shown, note image_tag is read-only
+    fields = ( 'image_tag','name','description','image','genre', 'address',)
+    readonly_fields = ('image_tag',)
+
 admin.site.register(Business)
-
-
-
-fields = ['image_tag']
-readonly_fields = ['image_tag']
