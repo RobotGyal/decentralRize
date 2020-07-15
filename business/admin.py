8e6f09ac2a3ba3e11cdaf97ec029be144d98d343
stasi815 <anastasia.gallardo@students.makeschool.com>
2020-07-15 00:24:14 -0700

    [add] render image in admin models

--- business/admin.py
+++ business/admin.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
-
+from django.utils.html import format_html
 from .models import Business
 
 admin.site.register(Business)
 
-fields = ['image_tag']
-readonly_fields = ['image_tag']
+@admin.register(Business) 
+class BusinessAdmin(admin.ModelAdmin):
+
+    def image_tag(self, obj):
+        return format_html('<img src="{}" />'.format(obj.image.url))
+
+    image_tag.short_description = 'Image'
+
+    list_display = ['image_tag',]
